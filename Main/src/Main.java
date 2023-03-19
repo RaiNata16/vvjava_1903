32b8e688dc1e0eb66ea46cd559ddd1e06b0d76e1
toncha <nata.toncha@mail.ru>
2023-03-19 19:23:32 +0300

    Main add gitignore

---
+++ Main/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 19;
+        int limm = 20;
+        int kmil = 0;
+        if (price >= limm) {
+            kmil = price / limm;
+        }
+        System.out.println("Количество миль " + kmil);
+    }
+}
